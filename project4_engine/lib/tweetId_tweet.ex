8b66260b25fbd785204ac850e1bd2bad0956cb59
JustAnirudh <anirudh.pathak1@gmail.com>
2017-11-29 22:17:47 -0500

    Add skeleton of tt Genserver

--- project4_engine/lib/tweetId_tweet.ex
+++ project4_engine/lib/tweetId_tweet.ex
@@ -1,4 +1,24 @@
 defmodule TweetIdTweet do
     use GenServer
-    
+
+    def init(state) do
+        :ets.new(:tt_table, [:set, :public, :named_table])
+        {:ok, state}
+    end
+
+    #insert/update
+    def handle_cast({:insert, curr_tweet_id, tweet, curr_time}, state) do
+        #TODO implement this
+        {:reply, :ok, state}
+    end
+
+    #get
+    def handle_call({:get, tweetId}, _from, state) do
+        #TODO implement this
+        {:reply, :ok, state}
+    end
+
+    def handle_info(_msg, state) do #catch unexpected messages
+        {:noreply, state}
+    end 
 end
