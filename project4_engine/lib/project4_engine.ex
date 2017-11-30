5de6ae68c765ff21934467e4e8865e9e41140373
JustAnirudh <anirudh.pathak1@gmail.com>
2017-11-30 04:18:16 -0500

    prototype cross node comm

--- project4_engine/lib/project4_engine.ex
+++ project4_engine/lib/project4_engine.ex
@@ -9,13 +9,12 @@ defmodule TwitterEngine do
   end
 
   def main(args) do
-    IO.inspect "hello"
 
     #starter process
     self() |> Process.register(:master)
 
     #5 tables
-    elem(GenServer.start_link(UserIdSubscribersSubscribedto, []), 1) |> Process.register(:uss)
+    elem(GenServer.start_link(UserIdSubscribedtoSubscribers, []), 1) |> Process.register(:uss)
     elem(GenServer.start_link(UserIdTweetIds, []), 1) |> Process.register(:ut)
     elem(GenServer.start_link(HashtagTweetIds, []), 1) |> Process.register(:ht)
     elem(GenServer.start_link(MentionTweetIds, []), 1) |> Process.register(:mt)
@@ -23,6 +22,18 @@ defmodule TwitterEngine do
 
     #engine
     elem(GenServer.start_link(Engine, {0,0}), 1) |> Process.register(:e)
+
+    #TODO: remove this: to check if cross node comm works
+    #epmd -daemon
+    master = self()
+    {:ok, _} = Node.start(String.to_atom("engine@127.0.0.1"))
+    app_name = :p4
+    Application.get_env(app_name, :cookie) |> Node.set_cookie #gets common cookie and sets the master's with it
+    :global.register_name(:master, master) #registers it for all connected nodes
+
+  receive do
+      {:from_client, msg} -> IO.inspect msg
+  end
 
     #loop infinitely
     loop()
--- project4_engine/mix.exs
+++ project4_engine/mix.exs
@@ -15,7 +15,8 @@ defmodule TwitterEngine.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      env: [cookie: :monster]
     ]
   end
 
