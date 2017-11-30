ee85ee330faf23129977df1e00dd0bee14bcc576
JustAnirudh <anirudh.pathak1@gmail.com>
2017-11-30 01:33:53 -0500

    prototype of client

--- project4_client/lib/project4_client.ex
+++ project4_client/lib/project4_client.ex
@@ -15,4 +15,18 @@ defmodule TwitterClient do
   def hello do
     :world
   end
+
+  def main(args) do
+    #epmd -daemon
+    {:ok, _} = Node.start(String.to_atom("client@127.0.0.1")) #converts current node to distributed node
+    app_name = :p4
+    Application.get_env(app_name, :cookie) |> Node.set_cookie #gets common cookie and sets the master's with it
+    #connect to master
+    _ = Node.connect(String.to_atom("engine@127.0.0.1")) #connect to master
+    :global.sync #sync global registry to let slave know of master being named :master
+    master_node_pid = :global.whereis_name(:master)
+    send master_node_pid, {:from_client, "hello from client"}
+
+    
+  end
 end
--- project4_client/mix.exs
+++ project4_client/mix.exs
@@ -7,15 +7,23 @@ defmodule TwitterClient.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      env: [cookie: :monster]
     ]
+  end
+
+  #Run as an executable
+  def escript do
+    [main_module: TwitterClient,
+    emu_args: [ "+P 5000000" ]]
   end
 
   # Run "mix help deps" to learn about dependencies.
